f1833202c762e004e7fbb37b40b82f8acae61fef
Mohammad Dinar <web.dev.dinar@gmail.com>
2025-03-06 15:15:12 +0600

    Created custom middlewares

---
+++ Middlewares/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+// in built middleware
+app.use(express.json())
+
+// const loggingMiddleware = (req, res, next)=>{
+// console.log("log korchi");
+// next();
+// }
+
+// const authMiddleware = (req, res, next)=>{
+//     console.log("auth korchi");
+    // res.send("Colo fherot colo")
+    // next();
+// }
+
+// const validationMiddleware = (req, res, next)=>{
+//     console.log("validation korchi");
+//     next();
+// }
+
+// use middlewares on app
+// app.use(loggingMiddleware);
+// app.use(authMiddleware);
+// app.use(validationMiddleware);
+
+app.get("/", (req, res)=>{
+    console.log(req.body)
+    res.send("Hello middlewares")
+});
+
+app.listen(port, ()=>{
+    console.log(`middleware server running on port ${port}`)
+})
